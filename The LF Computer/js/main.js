297298b15b102a441d1a04f91b1f7c970b118600
LuisFernandoGJ <34893711+LuisFernandoGJ@users.noreply.github.com>
2017-12-28 02:00:58 -0200

    Update main.js

--- The LF Computer/js/main.js
+++ The LF Computer/js/main.js
@@ -1,4 +1,4 @@
-var txtMemory, memory, letters, numbers, btmInsert, strChars, dwScope, scope, value1, value2, btnPut, btnStart, btnClear, inCode; // Variables
+var txtMemory, memory, letters, numbers, btmInsert, strChars, dwScope, scope, delay, value1, value2, btnPut, btnStart, btnClear, inCode; // Variables
 
 /*
 	Funtions
@@ -8,7 +8,8 @@ function Init(){
 	
 	// Html objects
 	txtMemory = document.createElement('p');
-	txtMemory.style = "font-family: consolas; text-align: center; top: 32px; font-size: 20px;"
+	txtMemory.style = "text-align: center; top: 32px; font-size: 20px;"
+	txtMemory.style.fontFamily = "consolas";
 	
 	btnPut = document.createElement('button');
 	btnPut.style = "position: absolute; top: 256px;"
@@ -43,12 +44,13 @@ function Init(){
 	
 	// Variables
 	memory = new Array(256);
-	letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+	letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "a", "b", "c", "d", "e", "f"];
 	numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 	strChars = new Array(memory.length);
 	dwScope = 0;
 	value1 = 0;
 	value2 = 0;
+	delay = 200;
 	
 	// Clear
 	Clear();
@@ -79,9 +81,12 @@ function Display(){
 function CharsUpdate(){
 	for(var i = 0; i < memory.length; i++){
 		
+		var num = memory[i];
+		//if ( num < 0 ){ num += 256; }
+		
 		var hexChar =
-			letters[ Math.floor(memory[i] / 16) ] +
-			letters[ memory[i] % 16 ];
+			letters[ Math.floor(num / 16) ] +
+			letters[ num % 16 ];
 			
 		
 		if ( i == dwScope ){
@@ -95,15 +100,23 @@ function CharsUpdate(){
 	
 }
 
-function PutCode(){
+function PutCode(){ // Put the code from input to memory
 	
-	var str = inCode.value;
+	var str = inCode.value; // Input string	
+	var char1 = ''; // First temporary char	
+	var i = 0; // Address counter
 	
-	for(var i = 0; i < Math.floor(str.length/2); i++){
+	for(var s = 0; s < str.length; s++){ // Input string counter
+	for(var j = 0; j < letters.length; j++){ // Test letters
+	if ( str.charAt(s) == letters[j] ){ // If char is a "letter"
+	
+		if ( (i%2) == 0 ){	char1 = str.charAt(s); }
+		else{ memory[(i-1)/2] = ByteToDec(char1, str.charAt(s)); }
 		
-		memory[i] = ByteToDec(str.charAt(i*2), str.charAt(i*2+1));
+		i++;
 		
-	}
+		break;	
+	}}}
 	
 	// Updates the code
 	CharsUpdate();
@@ -129,10 +142,26 @@ function ByteToDec(hex1, hex2){
 		if ( letters[i] == hex1 ){ // First hex
 			num += numbers[i]*16;
 		}
-		if ( letters[i] == hex2 ){ // Secont hex
+		if ( letters[i] == hex2 ){ // Second hex
 			num += numbers[i];
 		}
 	}
+	
+	for(var i = 16; i < 22; i++){ // Tests all letters
+		if ( letters[i] == hex1 ){ // First hex
+			//alert("1");
+			num += numbers[i-6]*16;
+		}
+		if ( letters[i] == hex2 ){ // Second hex
+			//alert("2");
+			num += numbers[i-6];
+		}
+	}
+	
+	//if ( num > 127 ){
+	//	num -= 256;
+	//}
+	
 	return num;
 }
 
@@ -143,23 +172,22 @@ function Computing(){
 			End();
 			break;
 		case 1:
-			setTimeout(Write(), 500);
+			setTimeout(Write(), delay);
 			break;
 		case 2:
-			setTimeout(Overwrite(), 500);
+			setTimeout(Overwrite(), delay);
 			break;
 		case 3:
-			setTimeout(Read(), 500);
+			setTimeout(Read(), delay);
+			break;
+		case 33:
+			setTimeout(Sum(), delay);
 			break;
 		default:
 			End();
 			break;
 	}
 	
-}
-
-function WaitFix(){
-	setTimeout(Computing(), 500);
 }
 
 /*
@@ -200,8 +228,8 @@ function Write(){
 	scope = (scope + 3) %256; 
 	dwScope = scope; // scope returns
 	CharsUpdate();
-	setTimeout( function() {Computing();}, 500);
-	}, 500);}, 500);}, 500);}, 500);
+	setTimeout( function() {Computing();}, delay);
+	}, delay);}, delay);}, delay);}, delay);
 	
 }
 
@@ -233,8 +261,8 @@ function Overwrite(){
 	scope = (scope + 3) %256;
 	dwScope = scope; // scope returns
 	CharsUpdate();
-	setTimeout( function() {Computing();}, 500);
-	}, 500);}, 500);}, 500);}, 500);}, 500);
+	setTimeout( function() {Computing();}, delay);
+	}, delay);}, delay);}, delay);}, delay);}, delay);
 	
 }
 
@@ -262,8 +290,41 @@ function Read(){
 	}
 	dwScope = scope; // If address1 is 01, then scope goes to value2
 	CharsUpdate();
-	setTimeout( function() {Computing();}, 500);
-	}, 500);}, 500);}, 500);
+	setTimeout( function() {Computing();}, delay);
+	}, delay);}, delay);}, delay);
+}
+
+function Sum(){
+	
+	value1 = memory[(scope+1) %256]; // Stores the value1
+	value2 = memory[(scope+2) %256]; // Stores the value2
+	
+	dwScope = (dwScope + 1) %256; // scope on value1
+	CharsUpdate();
+	setTimeout( function() {
+	
+	dwScope = (dwScope + 1) %256; // scope on value2
+	CharsUpdate();
+	setTimeout( function() {
+	
+	dwScope = value1; // scope on address1
+	CharsUpdate();
+	setTimeout( function() {
+	
+	dwScope = value2; // scope on address2
+	CharsUpdate();
+	setTimeout( function() {
+	
+	memory[value2] = (memory[value2] + memory[value1]) %256; // Change value
+	CharsUpdate();
+	setTimeout( function() {
+	
+	scope = (scope + 3) %256;
+	dwScope = scope; // scope returns
+	CharsUpdate();
+	setTimeout( function() {Computing();}, delay);
+	}, delay);}, delay);}, delay);}, delay);}, delay);
+	
 }
 
 // Main
